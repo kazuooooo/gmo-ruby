39548a31279466608e168e437f95c15e302ada43
kissy2go <kissy2go@gmail.com>
2015-11-05 15:19:00 +0900

    Migrate rspec to rspec-given DSL

--- spec/gmo_spec.rb
+++ spec/gmo_spec.rb
@@ -1,11 +1,5 @@
 require 'spec_helper'
 
 describe GMO do
-  it 'has a version number' do
-    expect(GMO::VERSION).not_to be nil
-  end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
+  Then { GMO::VERSION != nil }
 end
