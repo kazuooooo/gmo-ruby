f62f61cdea2569e01bece5b74741db7dba9235fd
kissy2go <kissy2go@gmail.com>
2015-11-10 17:09:26 +0900

    Communicate to GMO

--- lib/gmo/client.rb
+++ lib/gmo/client.rb
@@ -1,5 +1,17 @@
 module GMO
   class Client
+    # @param [Hash] options オプション
+    #
+    # @option options [String] :url エンドポイントURL
+    # @option options [Hash] :headers HTTPヘッダ
+    # @option options [Hash] :ssl SSLオプション
+    # @option options [Hash] :proxy プロキシオプション
+    #
+    # @see Faraday#new
+    def initialize(options)
+      @options = options
+    end
+
     # 取引登録
     # POST /payment/EntryTran.idPass
     #
@@ -19,7 +31,7 @@ module GMO
     #   @option payload [String] :TdFlag 本人認証サービス利用フラグ
     #   @option payload [String] :TdTenantName 本人認証サービス利用フラグ
     #
-    # @return [Hash] 取引登録結果
+    # @return [Faraday::Response] 取引登録結果
     def entry_tran(payload)
       # 2.1.  カード番号を入力して決済する＜本人認証サービスを未使用＞
       # 2.2.  カード番号を入力して決済する＜本人認証サービスを使用＞
@@ -29,7 +41,7 @@ module GMO
       #   [String] :AccessPass 取引パスワード
       #   [String] :ErrCode エラーコード
       #   [String] :ErrInfo エラー詳細コード
-      {}
+      conn.post '/payment/EntryTran.idPass', payload
     end
 
     # 決済実行
@@ -108,7 +120,7 @@ module GMO
     #   @option payload [String] :ClientField3 加盟店自由項目3
     #   @option payload [String] :ClientFieldFlag 加盟店自由項目返却フラグ
     #
-    # @return [Hash] 決済実行結果
+    # @return [Faraday::Response] 決済実行結果
     def exec_tran(payload)
       # 2.1.  カード番号を入力して決済する＜本人認証サービスを未使用＞
       # 2.10. 登録したカード情報で決済する＜本人認証サービスを未使用＞
@@ -133,7 +145,7 @@ module GMO
       #   [String] :ACSUrl 本人認証パスワード入力画面URL
       #   [String] :PaReq 本人認証要求電文
       #   [String] :MD 取引ID
-      {}
+      conn.post '/payment/ExecTran.idPass', payload
     end
 
     # 認証後決済実行
@@ -145,7 +157,7 @@ module GMO
     #   @option payload [String] :PaRes 本人認証サービス結果
     #   @option payload [String] :MD 取引ID
     #
-    # @return [Hash] 認証後決済実行結果
+    # @return [Faraday::Response] 認証後決済実行結果
     def secure_tran(payload)
       # 2.2. カード番号を入力して決済する＜本人認証サービスを使用＞
       #   [String] :OrderID オーダーID
@@ -161,7 +173,7 @@ module GMO
       #   [String] :ClientField3 加盟店自由項目3
       #   [String] :ErrCode エラーコード
       #   [String] :ErrInfo エラー詳細コード
-      {}
+      conn.post '/payment/SecureTran.idPass', payload
     end
 
     # 決済変更
@@ -195,7 +207,7 @@ module GMO
     #   @option payload [String] :JobCd 処理区分
     #   @option payload [Number] :Amount 利用金額
     #
-    # @return [Hash] 決済変更結果
+    # @return [Faraday::Response] 決済変更結果
     def alter_tran(payload)
       # 2.12. 決済の内容を取り消す
       # 2.13. 取り消した決済に再度オーソリを行う
@@ -208,7 +220,7 @@ module GMO
       #   [String] :TranDate 決済日付(yyyyMMddHHmmss形式)
       #   [String] :ErrCode エラーコード
       #   [String] :ErrInfo エラー詳細コード
-      {}
+      conn.post '/payment/AlterTran.idPass', payload
     end
 
     # 金額変更
@@ -225,7 +237,7 @@ module GMO
     #   @option payload [Number] :Amount 利用金額
     #   @option payload [Number] :Tax 税送料
     #
-    # @return [Hash] 金額変更結果
+    # @return [Faraday::Response] 金額変更結果
     def change_tran(payload)
       # 2.15. 完了した決済に金額の変更を行う
       #   [String] :AccessID 取引ID
@@ -236,7 +248,7 @@ module GMO
       #   [String] :TranDate 決済日付(yyyyMMddHHmmss形式)
       #   [String] :ErrCode エラーコード
       #   [String] :ErrInfo エラー詳細コード
-      {}
+      conn.post '/payment/ChangeTran.idPass', payload
     end
 
     # 取引状態参照
@@ -249,7 +261,7 @@ module GMO
     #   @option payload [String] :ShopPass ショップパスワード
     #   @option payload [String] :OrderID オーダーID
     #
-    # @return [Hash] 取引状態参照結果
+    # @return [Faraday::Response] 取引状態参照結果
     def search_trade(payload)
       # 2.16. 決済結果を参照する
       #   [String] :OrderID オーダーID
@@ -275,7 +287,7 @@ module GMO
       #   [String] :ClientField3 加盟店自由項目3
       #   [String] :ErrCode エラーコード
       #   [String] :ErrInfo エラー詳細コード
-      {}
+      conn.post '/payment/SearchTrade.idPass', payload
     end
 
     # 決済後カード登録
@@ -293,6 +305,8 @@ module GMO
     #   @option payload [String] :SeqMode カード登録連番モード
     #   @option payload [String] :DefaultFlag 洗替・継続課金フラグ
     #   @option payload [String] :HolderName 名義人
+    #
+    # @return [Faraday::Response] 決済後カード登録結果
     def traded_card(payload)
       # 2.17. カード番号決済に使用したカード番号を登録する
       #   [String] :CardSeq カード登録連番
@@ -300,7 +314,7 @@ module GMO
       #   [String] :Forward 仕向先コード
       #   [String] :ErrCode エラーコード
       #   [String] :ErrInfo エラー詳細コード
-      {}
+      conn.post '/payment/TradedCard.idPass', payload
     end
 
     # 会員登録
@@ -314,13 +328,13 @@ module GMO
     #   @option payload [String] :MemberID 会員ID
     #   @option payload [String] :MemberName 会員名
     #
-    # @return [Hash] 会員登録結果
+    # @return [Faraday::Response] 会員登録結果
     def save_member(payload)
       # 2.3. 会員情報を登録する
       #   [String] :MemberID 会員ID
       #   [String] :ErrCode エラーコード
       #   [String] :ErrInfo エラー詳細コード
-      {}
+      conn.post '/payment/SaveMember.idPass', payload
     end
 
     # 会員更新
@@ -334,13 +348,13 @@ module GMO
     #   @option payload [String] :MemberID 会員ID
     #   @option payload [String] :MemberName 会員名
     #
-    # @return [Hash] 会員更新結果
+    # @return [Faraday::Response] 会員更新結果
     def update_member(payload)
       # 2.4. 会員情報を更新する
       #   [String] :MemberID 会員ID
       #   [String] :ErrCode エラーコード
       #   [String] :ErrInfo エラー詳細コード
-      {}
+      conn.post '/payment/UpdateMember.idPass', payload
     end
 
     # 会員削除
@@ -353,13 +367,13 @@ module GMO
     #   @option payload [String] :SitePass サイトパスワード
     #   @option payload [String] :MemberID 会員ID
     #
-    # @return [Hash] 会員削除結果
+    # @return [Faraday::Response] 会員削除結果
     def delete_member(payload)
       # 2.5. 会員情報を削除する
       #   [String] :MemberID 会員ID
       #   [String] :ErrCode エラーコード
       #   [String] :ErrInfo エラー詳細コード
-      {}
+      conn.post '/payment/DeleteMember.idPass', payload
     end
 
     # 会員参照
@@ -372,7 +386,7 @@ module GMO
     #   @option payload [String] :SitePass サイトパスワード
     #   @option payload [String] :MemberID 会員ID
     #
-    # @return [Hash] 会員参照結果
+    # @return [Faraday::Response] 会員参照結果
     def search_member(payload)
       # 2.6. 会員情報を参照する
       #   [String] :MemberID 会員ID
@@ -380,7 +394,7 @@ module GMO
       #   [String] :DeleteFlag 削除フラグ
       #   [String] :ErrCode エラーコード
       #   [String] :ErrInfo エラー詳細コード
-      {}
+      conn.post '/payment/SearchMember.idPass', payload
     end
 
     # カード登録／更新
@@ -401,7 +415,7 @@ module GMO
     #   @option payload [String] :Expire 有効期限
     #   @option payload [String] :HolderName 名義人
     #
-    # @return [Hash] カード登録／更新結果
+    # @return [Faraday::Response] カード登録／更新結果
     def save_card(payload)
       # 2.7. カード譲歩を登録または更新する
       #   [String] :CardSeq カード登録連番
@@ -409,7 +423,7 @@ module GMO
       #   [String] :Forward 仕向先コード
       #   [String] :ErrCode エラーコード
       #   [String] :ErrInfo エラー詳細コード
-      {}
+      conn.post '/payment/SaveCard.idPass', payload
     end
 
     # カード削除
@@ -424,13 +438,13 @@ module GMO
     #   @option payload [String] :SeqMode カード登録連番モード
     #   @option payload [Number] :CardSeq カード登録連番
     #
-    # @return [Hash] カード削除結果
+    # @return [Faraday::Response] カード削除結果
     def delete_card(payload)
       # 2.8. カード情報の削除をする
       #   [String] :CardSeq カード登録連番
       #   [String] :ErrCode エラーコード
       #   [String] :ErrInfo エラー詳細コード
-      {}
+      conn.post '/payment/DeleteCard.idPass', payload
     end
 
     # カード参照
@@ -447,7 +461,7 @@ module GMO
     #   @option payload [String] :SeqMode カード登録連番モード
     #   @option payload [Number] :CardSeq カード登録連番
     #
-    # @return [Hash] カード参照結果
+    # @return [Faraday::Response] カード参照結果
     def search_card(payload)
       # 2.9.  カード情報を参照する
       # 2.10. 登録したカード情報で決済する＜本人認証サービスを未使用＞
@@ -461,7 +475,17 @@ module GMO
       #   [String] :DeleteFlag 削除フラグ
       #   [String] :ErrCode エラーコード
       #   [String] :ErrInfo エラー詳細コード
-      {}
+      conn.post '/payment/SearchCard.idPass', payload
+    end
+
+    private
+
+    # @return [Faraday::Connection] 通信コネクション
+    def conn
+      @conn ||= Faraday.new(@options) { |conn|
+        conn.request :url_encoded
+        conn.adapter Faraday.default_adapter
+      }
     end
   end
 end
--- spec/gmo/client_spec.rb
+++ spec/gmo/client_spec.rb
@@ -1,89 +1,92 @@
 require 'spec_helper'
 
 describe GMO::Client do
-  Given(:client) { described_class.new }
+  Given(:client_options) { {
+    url: ENV['GMO_URL'],
+  } }
+  Given(:client) { described_class.new(client_options) }
 
   context '#entry_tran' do
     Given(:payload) { {} }
     When (:result) { client.entry_tran(payload) }
-    Then { result.is_a? Hash }
+    Then { result.is_a? Faraday::Response }
   end
 
   context '#exec_tran' do
     Given(:payload) { {} }
     When (:result) { client.exec_tran(payload) }
-    Then { result.is_a? Hash }
+    Then { result.is_a? Faraday::Response }
   end
 
   context '#secure_tran' do
     Given(:payload) { {} }
     When (:result) { client.secure_tran(payload) }
-    Then { result.is_a? Hash }
+    Then { result.is_a? Faraday::Response }
   end
 
   context '#alter_tran' do
     Given(:payload) { {} }
     When (:result) { client.alter_tran(payload) }
-    Then { result.is_a? Hash }
+    Then { result.is_a? Faraday::Response }
   end
 
   context '#change_tran' do
     Given(:payload) { {} }
     When (:result) { client.change_tran(payload) }
-    Then { result.is_a? Hash }
+    Then { result.is_a? Faraday::Response }
   end
 
   context '#search_trade' do
     Given(:payload) { {} }
     When (:result) { client.search_trade(payload) }
-    Then { result.is_a? Hash }
+    Then { result.is_a? Faraday::Response }
   end
 
   context '#traded_card' do
     Given(:payload) { {} }
     When (:result) { client.traded_card(payload) }
-    Then { result.is_a? Hash }
+    Then { result.is_a? Faraday::Response }
   end
 
   context '#save_member' do
     Given(:payload) { {} }
     When (:result) { client.save_member(payload) }
-    Then { result.is_a? Hash }
+    Then { result.is_a? Faraday::Response }
   end
 
   context '#update_member' do
     Given(:payload) { {} }
     When (:result) { client.update_member(payload) }
-    Then { result.is_a? Hash }
+    Then { result.is_a? Faraday::Response }
   end
 
   context '#delete_member' do
     Given(:payload) { {} }
     When (:result) { client.delete_member(payload) }
-    Then { result.is_a? Hash }
+    Then { result.is_a? Faraday::Response }
   end
 
   context '#search_member' do
     Given(:payload) { {} }
     When (:result) { client.search_member(payload) }
-    Then { result.is_a? Hash }
+    Then { result.is_a? Faraday::Response }
   end
 
   context '#save_card' do
     Given(:payload) { {} }
     When (:result) { client.save_card(payload) }
-    Then { result.is_a? Hash }
+    Then { result.is_a? Faraday::Response }
   end
 
   context '#delete_card' do
     Given(:payload) { {} }
     When (:result) { client.delete_card(payload) }
-    Then { result.is_a? Hash }
+    Then { result.is_a? Faraday::Response }
   end
 
   context '#search_card' do
     Given(:payload) { {} }
     When (:result) { client.search_card(payload) }
-    Then { result.is_a? Hash }
+    Then { result.is_a? Faraday::Response }
   end
 end
